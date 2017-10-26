da670df0715537476b7b1601331e7b111208d071
Remi Duplan <duplan.remi@gmail.com>
2017-10-26 15:42:56 -0400

    1st commit

---
+++ WindowsFormsAppEntityFrameWork/WindowsFormsAppEntityFrameWork/FormEmploye.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsAppEntityFrameWork
+{
+    partial class FormEmployeeProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSauvegarder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNoEmploye = new System.Windows.Forms.TextBox();
+            this.textBoxCourriel = new System.Windows.Forms.TextBox();
+            this.textBoxTelephone = new System.Windows.Forms.TextBox();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.buttonModifier = new System.Windows.Forms.Button();
+            this.buttonSupprimer = new System.Windows.Forms.Button();
+            this.buttonAfficherEmp = new System.Windows.Forms.Button();
+            this.buttonRechercher = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridViewEmploye = new System.Windows.Forms.DataGridView();
+            this.buttonQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmploye)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSauvegarder
+            // 
+            this.buttonSauvegarder.Location = new System.Drawing.Point(43, 131);
+            this.buttonSauvegarder.Name = "buttonSauvegarder";
+            this.buttonSauvegarder.Size = new System.Drawing.Size(75, 23);
+            this.buttonSauvegarder.TabIndex = 0;
+            this.buttonSauvegarder.Text = "Sauvegarder";
+            this.buttonSauvegarder.UseVisualStyleBackColor = true;
+            this.buttonSauvegarder.Click += new System.EventHandler(this.buttonSauvegarder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No Employee";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prenom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(316, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telephone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(272, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Courriel";
+            // 
+            // textBoxNoEmploye
+            // 
+            this.textBoxNoEmploye.Location = new System.Drawing.Point(67, 43);
+            this.textBoxNoEmploye.Name = "textBoxNoEmploye";
+            this.textBoxNoEmploye.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNoEmploye.TabIndex = 6;
+            // 
+            // textBoxCourriel
+            // 
+            this.textBoxCourriel.Location = new System.Drawing.Point(275, 96);
+            this.textBoxCourriel.Name = "textBoxCourriel";
+            this.textBoxCourriel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCourriel.TabIndex = 7;
+            // 
+            // textBoxTelephone
+            // 
+            this.textBoxTelephone.Location = new System.Drawing.Point(112, 96);
+            this.textBoxTelephone.Name = "textBoxTelephone";
+            this.textBoxTelephone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTelephone.TabIndex = 8;
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(325, 43);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNom.TabIndex = 9;
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Location = new System.Drawing.Point(190, 43);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrenom.TabIndex = 10;
+            // 
+            // buttonModifier
+            // 
+            this.buttonModifier.Location = new System.Drawing.Point(155, 131);
+            this.buttonModifier.Name = "buttonModifier";
+            this.buttonModifier.Size = new System.Drawing.Size(75, 23);
+            this.buttonModifier.TabIndex = 11;
+            this.buttonModifier.Text = "Modifier";
+            this.buttonModifier.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupprimer
+            // 
+            this.buttonSupprimer.Location = new System.Drawing.Point(249, 131);
+            this.buttonSupprimer.Name = "buttonSupprimer";
+            this.buttonSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.buttonSupprimer.TabIndex = 12;
+            this.buttonSupprimer.Text = "Supprimer";
+            this.buttonSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // buttonAfficherEmp
+            // 
+            this.buttonAfficherEmp.Location = new System.Drawing.Point(350, 131);
+            this.buttonAfficherEmp.Name = "buttonAfficherEmp";
+            this.buttonAfficherEmp.Size = new System.Drawing.Size(122, 23);
+            this.buttonAfficherEmp.TabIndex = 13;
+            this.buttonAfficherEmp.Text = "Afficher Employes";
+            this.buttonAfficherEmp.UseVisualStyleBackColor = true;
+            this.buttonAfficherEmp.Click += new System.EventHandler(this.buttonAfficherEmp_Click);
+            // 
+            // buttonRechercher
+            // 
+            this.buttonRechercher.Location = new System.Drawing.Point(397, 224);
+            this.buttonRechercher.Name = "buttonRechercher";
+            this.buttonRechercher.Size = new System.Drawing.Size(75, 23);
+            this.buttonRechercher.TabIndex = 14;
+            this.buttonRechercher.Text = "Rechercher";
+            this.buttonRechercher.UseVisualStyleBackColor = true;
+            this.buttonRechercher.Click += new System.EventHandler(this.buttonRechercher_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Rechercher par";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "No Employe",
+            "Prenom",
+            "Nom"});
+            this.comboBox1.Location = new System.Drawing.Point(169, 224);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // dataGridViewEmploye
+            // 
+            this.dataGridViewEmploye.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmploye.Location = new System.Drawing.Point(43, 279);
+            this.dataGridViewEmploye.Name = "dataGridViewEmploye";
+            this.dataGridViewEmploye.Size = new System.Drawing.Size(429, 89);
+            this.dataGridViewEmploye.TabIndex = 18;
+            // 
+            // buttonQuitter
+            // 
+            this.buttonQuitter.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonQuitter.Location = new System.Drawing.Point(446, 389);
+            this.buttonQuitter.Name = "buttonQuitter";
+            this.buttonQuitter.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuitter.TabIndex = 19;
+            this.buttonQuitter.Text = "Quitter";
+            this.buttonQuitter.UseVisualStyleBackColor = true;
+            this.buttonQuitter.Click += new System.EventHandler(this.buttonQuitter_Click);
+            // 
+            // FormEmployeeProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonQuitter;
+            this.ClientSize = new System.Drawing.Size(547, 437);
+            this.Controls.Add(this.buttonQuitter);
+            this.Controls.Add(this.dataGridViewEmploye);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonRechercher);
+            this.Controls.Add(this.buttonAfficherEmp);
+            this.Controls.Add(this.buttonSupprimer);
+            this.Controls.Add(this.buttonModifier);
+            this.Controls.Add(this.textBoxPrenom);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.textBoxTelephone);
+            this.Controls.Add(this.textBoxCourriel);
+            this.Controls.Add(this.textBoxNoEmploye);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSauvegarder);
+            this.Name = "FormEmployeeProject";
+            this.Text = "Entity Framework Application";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmploye)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSauvegarder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxNoEmploye;
+        private System.Windows.Forms.TextBox textBoxCourriel;
+        private System.Windows.Forms.TextBox textBoxTelephone;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textBoxPrenom;
+        private System.Windows.Forms.Button buttonModifier;
+        private System.Windows.Forms.Button buttonSupprimer;
+        private System.Windows.Forms.Button buttonAfficherEmp;
+        private System.Windows.Forms.Button buttonRechercher;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridViewEmploye;
+        private System.Windows.Forms.Button buttonQuitter;
+    }
+}
+
---
+++ WindowsFormsAppEntityFrameWork/WindowsFormsAppEntityFrameWork/FormEmploye.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEntityFrameWork
+{
+    public partial class FormEmployeeProject : Form
+    {
+        EmployeProjetEntities empProjet = new EmployeProjetEntities();
+        public FormEmployeeProject()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonSauvegarder_Click(object sender, EventArgs e)
+        {
+            //Valider des donnees 
+
+            Employe emp = new Employe();
+            emp.NoEmploye = Convert.ToInt32(textBoxNoEmploye.Text);
+            emp.Prenom = textBoxPrenom.Text;
+            emp.Nom = textBoxNom.Text;
+            emp.Telephone = textBoxTelephone.Text;
+            emp.Courriel = textBoxCourriel.Text;
+            empProjet.Employes.Add(emp);
+            empProjet.SaveChanges();
+            MessageBox.Show("Sauvegarde avec succes.");
+
+
+        }
+
+        private void buttonRechercher_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBoxNoEmploye.Text);
+
+            //LINQ to entity
+            var employe = (from emp in empProjet.Employes
+                           where emp.NoEmploye == id
+                           select emp).Single();
+        }
+
+        private void buttonAfficherEmp_Click(object sender, EventArgs e)
+        {
+            var listEmp = (from emp in empProjet.Employes
+                           select emp).ToList();
+            dataGridViewEmploye.DataSource = listEmp;
+            dataGridViewEmploye.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+    }
+}
